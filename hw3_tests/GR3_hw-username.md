f36f8598f584cd9bca612dc5437560bdb6d2fa71
Zhihan Chen <zhihanch@usc.edu>
2022-11-29 04:32:02 +0000

    All done

---
+++ hw3_tests/GR3_hw-username.md
@@ -0,0 +1,20 @@
+## HW 03 Test Case Grade Report
+
+This is the score breakdown:
+
+### Problem 3 (llrec) (19.0/23):
+
++ (-4.0 points) Valgrind error
+
+### Problem 4 (stack) (2/2):
+
+### Problem 7 (heap) (25/25):
+
+### Other Deductions & Penalties:
+
+### Total Score:
+
++ (19.0 + 2 + 25) - (0) = **46.0/50 points**
+
+
+
--- stack.h
+++ stack.h
@@ -1,3 +1,80 @@
+// #ifndef STACK_H
+// #define STACK_H
+
+// #include <vector>
+// #include <stdexcept>
+
+// // Use inheritance from std::vector (choose public/private) as appropriate
+// template <typename T>
+// class Stack : private std::vector<T>
+// {
+// public:
+//     Stack();
+//     ~Stack();
+//     bool empty() const;
+//     size_t size() const;
+//     void push(const T& item);
+//     void pop();  // throws std::underflow_error if empty
+//     const T& top() const; // throws std::underflow_error if empty
+//     // Add other members only if necessary
+
+// private:
+// 		size_t size_;
+// };
+
+// template <typename T>
+// Stack<T>::Stack(){
+// 	size_ = 0;
+	
+// }
+
+// template <typename T>
+// Stack<T>::~Stack(){
+// 	while(Stack<T>::empty()==false){
+// 		Stack<T>::pop();
+// 	}
+// }
+
+// template <typename T>
+// bool Stack<T>::empty() const{
+// 	return std::vector<T>::empty();
+// }
+
+// template <typename T>
+// size_t Stack<T>::size() const{
+// 	// return std::vector<T>::size();
+// 	return size_;
+// }
+
+// template <typename T>
+// void Stack<T>::push(const T& item){
+// 	size_++;
+// 	std::vector<T>::push_back(item);
+// }
+
+// template <typename T>
+// void Stack<T>::pop(){ 
+// 	if(Stack<T>::empty()){
+// 		throw std::underflow_error("empty");
+// 	}
+// 	else{
+// 		std::vector<T>::vec.pop_back();
+// 		size_--;
+// 	}
+
+// }
+// template <typename T>
+// const T& Stack<T>::top() const{ 
+// 	if(Stack<T>::empty()){
+// 		throw std::underflow_error("empty");
+// 	}
+// 	else{
+// 		return (*this)[std::vector<T>::size()-1];
+// 	}
+// }
+
+// #endif
+
 #ifndef STACK_H
 #define STACK_H
 
@@ -18,58 +95,57 @@ public:
     const T& top() const; // throws std::underflow_error if empty
     // Add other members only if necessary
 
-private:
-		size_t size_;
+    private:
+   
 };
 
-template <typename T>
-Stack<T>::Stack()::std::vector<T>{
-	size_ = 0;
-	
+template<typename T>
+Stack<T> :: Stack(){
+   
 }
 
-template <typename T>
-Stack<T>::~Stack(){
-	while(Stack<T>::empty()){
+template<typename T>
+Stack<T> :: ~Stack(){ 
+	while(!Stack<T>::empty()){
 		Stack<T>::pop();
+ }
+}
+
+template<typename T>
+bool Stack<T>:: empty() const{ 
+  return std::vector<T>::empty();
+}
+
+template<typename T>
+size_t Stack<T>::size()const{ 
+	return std::vector<T>::size();
+}
+
+template<typename T>
+void Stack<T>::push(const T& item){ 
+  std::vector<T>::push_back(item); 
+}
+
+template<typename T> 
+void Stack<T>::pop(){ 
+	if(empty()){
+  	throw std::underflow_error("empty");
+ }
+	else{
+    std::vector<T>::pop_back();
 	}
 }
 
-template <typename T>
-bool Stack<T>::empty() const{
-	return std::vector<T>::empty();
-}
 
-template <typename T>
-size_t Stack<T>::size() const{
-	return size_;
-}
-
-template <typename T>
-void Stack<T>::push(const T& item){
-	size_++;
-	std::vector<T>::push_back(item);
-}
-
-template <typename T>
-void Stack<T>:: pop(){ 
-	if(Stack<T>::empty()){
+template<typename T>
+const T& Stack<T> ::top()const{
+  if(!empty()) return (*this)[std::vector<T>::size()-1];
+   
+  else{
 		throw std::underflow_error("empty");
 	}
-	else{
-		std::vector<T>::vec.pop_back();
-		size_--;
-	}
+}
 
-}
-template <typename T>
-const Stack<T>:: T& top() const{ 
-	if(Stack<T>::empty()){
-		throw std::underflow_error("empty");
-	}
-	else{
-		return (*this)[std::vector<T>::size()-1];
-	}
-}
+
 
 #endif
